ed3f8817481a85b2acbb93a276fb2d73062be66b
Vladimir Teplyakov <vteplakov@gmail.com>
2023-06-24 03:15:58 +0600

    homework with script

---
+++ myScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd papka
+mkdir papka_1 papka_2 papka_3
+cd papka_1
+touch 1.txt 2.txt 3.txt 4.json 5.json
+mkdir papka_4 papka_5 papka_6
+ls -la
+mv 1.txt 2.txt ~/Desktop/choke/papka/papka_2
